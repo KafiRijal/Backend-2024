a50ba07fa5f8a2ddf906d76c8d7913d7283e8c66
KafiRijal <kafirs75@gmail.com>
2025-01-15 16:14:18 +0700

    UAS

--- controllers/AlumniController.js
+++ controllers/AlumniController.js
@@ -3,24 +3,30 @@ const Alumni = require("../models/Alumni");
 
 // buat class AlumniController
 class AlumniController {
-  // fungsi untuk mendapatkan semua data alumni
+  // Fungsi untuk mendapatkan semua data alumni
   async index(req, res) {
+    // Menggunakan method "all()" dari model Alumni untuk mengambil semua data
     const alumni = await Alumni.all();
-    if (alumni && alumni.length > 0) {
-      const data = {
-        message: "Get All Resource",
-        data: alumni,
-      };
-      return res.status(200).json(data);
-    } else {
+    // Jika data alumni kosong atau tidak ditemukan
+    if (!alumni || alumni.length === 0) {
+      // Jika data kosong, mengirim respons status 404
       const data = {
         message: "Data is empty",
       };
-      return res.status(200).json(data);
+      return res.status(404).json(data);
     }
+
+    // Jika data ditemukan, mengirim respons status 200
+    const data = {
+      message: "Get All Resource",
+      data: alumni,
+    };
+
+    return res.status(200).json(data);
   }
-  // fungsi untuk menambahkan data alumni
+  // Fungsi untuk menambahkan data alumni
   async store(req, res) {
+    // Mengambil data yang dikirim melalui body request
     const {
       name,
       phone,
@@ -31,7 +37,7 @@ class AlumniController {
       position,
     } = req.body;
 
-    //validasi input data
+    // Validasi input data
     if (
       !name ||
       !phone ||
@@ -41,13 +47,15 @@ class AlumniController {
       !company_name ||
       !position
     ) {
+      // Jika ada field yang kosong, mengirim respons status 422
       const data = {
         message: "All fields must be filled correctly",
       };
       return res.status(422).json(data);
     }
-    // menambahkan data alumni ke database
+    // Menambahkan data baru menggunakan method "create()" dari model Alumni
     const alumni = await Alumni.create(req.body);
+    // Jika data berhasil ditambahkan, mengirim respons status 201
     const data = {
       message: "Resource is added successfully",
       data: alumni,
@@ -55,138 +63,170 @@ class AlumniController {
     return res.status(201).json(data);
   }
 
-  // fungsi untuk mengupdate data alumni menggunakan parameter id
+  // Fungsi untuk mengupdate data alumni menggunakan parameter id
   async update(req, res) {
+    // Mendapatkan id dari parameter
     const { id } = req.params;
-    // mencari id alumni yang mau diupdate
+    // Mencari id alumni yang mau diupdate
     const alumni = await Alumni.find(id);
 
-    if (alumni) {
-      // melakukan update data
-      const alumni = await Alumni.update(id, req.body);
+    if (!alumni) {
+      // Jika data tidak ditemukan, mengirim respons status 404
       const data = {
-        message: `Resource is updated successfully`,
-        data: alumni,
-      };
-      res.status(200).json(data);
-    } else {
-      const data = {
-        message: `Resource not found`,
+        message: "Resource not found",
       };
       res.status(404).json(data);
     }
+    // Memperbarui data alumni berdasarkan id dan data baru dari body request
+    const update_alumni = await Alumni.update(id, req.body);
+    // Jika data berhasil diupdate, mengirim respons status 200
+    const data = {
+      message: "Resource is updated successfully",
+      data: update_alumni,
+    };
+    res.status(200).json(data);
   }
 
-  // fungsi untuk menghapus data alumni menggunakan parameter id
+  // Fungsi untuk menghapus data alumni menggunakan parameter id
   async destroy(req, res) {
+    // Mendapatkan id dari parameter
     const { id } = req.params;
-    // mencari id alumni yang mau dihapus
+    // Mencari id alumni yang mau dihapus
     const alumni = await Alumni.find(id);
 
     if (alumni) {
-      // melakukan hapus data
-      await Alumni.delete(id);
+      // Jika data tidak ditemukan, mengirim respons status 404
       const data = {
-        message: `Resource is delete successfully`,
-      };
-      res.status(200).json(data);
-    } else {
-      const data = {
-        message: `Resource not found`,
+        message: "Resource not found",
       };
       res.status(404).json(data);
     }
+    // Menghapus data alumni berdasarkan id
+    await Alumni.delete(id);
+    // Jika data berhasil dihapus, mengirim respons status 200
+    const data = {
+      message: "Resource is delete successfully",
+    };
+    res.status(200).json(data);
   }
 
-  // fungsi untuk menampilkan detail data alumni menggunakan parameter id
+  // Fungsi untuk menampilkan detail data alumni menggunakan parameter id
   async show(req, res) {
+    // Mendapatkan id dari parameter
     const { id } = req.params;
-    // mencari id alumni yang mau ditampilkan
+    // Mencari id alumni yang mau ditampilkan
     const alumni = await Alumni.find(id);
 
-    if (alumni) {
+    if (!alumni) {
+      // Jika data tidak ditemukan, mengirim respons status 404
       const data = {
-        message: `Get Detail Resource`,
-        data: alumni,
-      };
-      res.status(200).json(data);
-    } else {
-      const data = {
-        message: `Resource not found`,
+        message: "Resource not found",
       };
       res.status(404).json(data);
     }
+    // Jika data berhasil ditemukan, mengirim respons status 200
+    const data = {
+      message: "Get Detail Resource",
+      data: alumni,
+    };
+    res.status(200).json(data);
   }
 
-  // fungsi untuk mencari data alumni menggunakan parameter nama
+  // Fungsi untuk mencari data alumni menggunakan parameter nama
   async search(req, res) {
+    // Mendapatkan nama dari parameter
     const { name } = req.params;
-    // mencari nama alumni yang mau ditampilkan
+    // Mencari nama alumni yang mau ditampilkan
     const alumni = await Alumni.search(name);
 
-    if (alumni) {
+    if (!alumni) {
+      // Jika data tidak ditemukan, mengirim respons status 404
       const data = {
-        message: `Get Searched Resource`,
-        data: alumni,
-      };
-      res.status(200).json(data);
-    } else {
-      const data = {
-        message: `Resource not found`,
+        message: "Resource not found",
       };
       res.status(404).json(data);
     }
+    // Jika data berhasil ditemukan, mengirim respons status 200
+    const data = {
+      message: "Get Searched Resource",
+      data: alumni,
+    };
+    res.status(200).json(data);
   }
 
-  // fungsi untuk mendapatkan data alumni dengan status fresh graduate
+  // Fungsi untuk mendapatkan data alumni dengan status fresh graduate
   async freshGraduate(req, res) {
+    // Mendapatkan status fresh-graduate
     const status = "fresh-graduate";
-    // mencari status alumni yang mau ditampilkan
+    // Mencari status alumni yang mau ditampilkan
     const alumni = await Alumni.findByStatus(status);
+    // Menghitung total status alumni yang ditampilkan
     const total_alumni = await Alumni.countByStatus(status);
 
-    if (alumni) {
+    if (!alumni) {
+      // Jika data tidak ditemukan, mengirim respons status 404
       const data = {
-        message: `Get fresh graduate resource`,
-        data: alumni,
-        total: total_alumni,
+        message: "Resource not found",
       };
-      res.status(200).json(data);
+      res.status(404).json(data);
     }
+    // Jika data berhasil ditemukan, mengirim respons status 200
+    const data = {
+      message: "Get Searched Resource",
+      data: alumni,
+      total: total_alumni,
+    };
+    res.status(200).json(data);
   }
 
-  // fungsi untuk mendapatkan data alumni dengan status employed
+  // Fungsi untuk mendapatkan data alumni dengan status employed
   async employed(req, res) {
+    // Mendapatkan status employed
     const status = "employed";
-    // mencari status alumni yang mau ditampilkan
+    // Mencari status alumni yang mau ditampilkan
     const alumni = await Alumni.findByStatus(status);
+    // Menghitung total status alumni yang ditampilkan
     const total_alumni = await Alumni.countByStatus(status);
 
-    if (alumni) {
+    if (!alumni) {
+      // Jika data tidak ditemukan, mengirim respons status 404
       const data = {
-        message: `Get employed resource`,
-        data: alumni,
-        total: total_alumni,
+        message: "Resource not found",
       };
-      res.status(200).json(data);
+      res.status(404).json(data);
     }
+    // Jika data berhasil ditemukan, mengirim respons status 200
+    const data = {
+      message: "Get Searched Resource",
+      data: alumni,
+      total: total_alumni,
+    };
+    res.status(200).json(data);
   }
 
-  // fungsi untuk mendapatkan data alumni dengan status unemployed
+  // Fungsi untuk mendapatkan data alumni dengan status unemployed
   async unemployed(req, res) {
+    // Mendapatkan status employed
     const status = "unemployed";
-    // mencari status alumni yang mau ditampilkan
+    // Mencari status alumni yang mau ditampilkan
     const alumni = await Alumni.findByStatus(status);
+    // Menghitung total status alumni yang ditampilkan
     const total_alumni = await Alumni.countByStatus(status);
 
-    if (alumni) {
+    if (!alumni) {
+      // Jika data tidak ditemukan, mengirim respons status 404
       const data = {
-        message: `Get unemployed resource`,
-        data: alumni,
-        total: total_alumni,
+        message: "Resource not found",
       };
-      res.status(200).json(data);
+      res.status(404).json(data);
     }
+    // Jika data berhasil ditemukan, mengirim respons status 200
+    const data = {
+      message: "Get Searched Resource",
+      data: alumni,
+      total: total_alumni,
+    };
+    res.status(200).json(data);
   }
 }
 
